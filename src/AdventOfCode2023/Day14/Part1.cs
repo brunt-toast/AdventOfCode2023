886ef807ddca3439d9d8b3cf12613c9de1ad229c
Josh Brunton <josh.brunton@proton.me>
2023-12-15 14:32:06 +0000

    Create files for days not yet done

---
+++ src/AdventOfCode2023/Day14/Part1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.TestSupport;
+
+namespace AdventOfCode2023.Day14
+{
+    public class Part1 : IAocAnswer
+    {
+        private StreamReader _stream = new(@"Day12/input.txt");
+        private readonly string[] _input;
+
+        public Part1()
+        {
+            _input = _stream.ReadToEnd().Split("\r\n");
+        }
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AdventOfCode2023/Day15/Part2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode2023.TestSupport;
+
+namespace AdventOfCode2023.Day15
+{
+    internal class Part2 : IAocAnswer
+    {
+        private StreamReader _stream = new(@"Day15/input.txt");
+        private readonly string[] _input;
+
+        public Part2()
+        {
+            _input = _stream.ReadToEnd().Split(',');
+        }
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
