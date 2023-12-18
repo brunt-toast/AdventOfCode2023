2f3e5cdc2f926fea36277203077255c20a82619b
Josh Brunton <josh.brunton@proton.me>
2023-12-18 16:06:38 +0000

    Look ahead up to day 24

---
+++ src/AdventOfCode2023/Day20/Part2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.TestSupport;
+
+namespace AdventOfCode2023.Day20
+{
+    internal class Part2 : IAocAnswer
+    {
+        private StreamReader _stream = new(@"Day20/input.txt");
+        private readonly string[] _input;
+
+        public Part2()
+        {
+            _input = _stream.ReadToEnd().Split("\r\n");
+        }
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AdventOfCode2023/Day24/Part1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.TestSupport;
+
+namespace AdventOfCode2023.Day24
+{
+    internal class Part1 : IAocAnswer
+    {
+        private StreamReader _stream = new(@"Day24/input.txt");
+        private readonly string[] _input;
+
+        public Part1()
+        {
+            _input = _stream.ReadToEnd().Split("\r\n");
+        }
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
