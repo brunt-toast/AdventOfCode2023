92a171ba674df13e7384f34b6402b37556f6be2c
Josh Brunton <josh.brunton@proton.me>
2023-12-18 15:44:14 +0000

    Basic for days 16-18

---
+++ src/AdventOfCode2023/Day17/Part1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2023.Day05;
+using AdventOfCode2023.TestSupport;
+
+namespace AdventOfCode2023.Day17
+{
+    internal class Part1 : IAocAnswer
+    {
+        private StreamReader _stream = new(@"Day17/input.txt");
+        private readonly string[] _input;
+
+        public Part1()
+        {
+            _input = _stream.ReadToEnd().Split("\r\n");
+        }
+
+        public long Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
